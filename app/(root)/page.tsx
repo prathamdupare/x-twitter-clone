10bd546f90afd5e14f734cde1b0265df508e7192
prathamdupare <prathamdupare@proton.me>
2024-03-19 12:18:21 +0530

    added authentication using clerk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <main>Threads</main>;
+  return (
+    <>
+      <h1 className="head-text text-left ">Home</h1>
+    </>
+  );
 }
