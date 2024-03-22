0c92a3c9abce3ae4d7225fdf6b19d700c77ed558
prathamdupare <prathamdupare@proton.me>
2024-03-22 21:38:50 +0530

    removed communities, added searchbar

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -21,11 +21,14 @@ import { ThreadValidation } from "@/lib/validations/thread";
 import { createThread } from "@/lib/actions/thread.action";
 
 interface Props {
-  userId: string; // Update the type definition
+  userId: string;
 }
+
 function PostThread({ userId }: Props) {
   const router = useRouter();
   const pathname = usePathname();
+
+  const { organization } = useOrganization();
 
   const form = useForm<z.infer<typeof ThreadValidation>>({
     resolver: zodResolver(ThreadValidation),
@@ -39,7 +42,7 @@ function PostThread({ userId }: Props) {
     await createThread({
       text: values.thread,
       author: userId,
-      communityId: null,
+      communityId: organization ? organization.id : null,
       path: pathname,
     });
 
--- lib/models/community.model.ts
+++ lib/models/community.model.ts
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import { boolean } from "zod";
 
 const communitySchema = new mongoose.Schema({
   id: {
@@ -8,16 +7,14 @@ const communitySchema = new mongoose.Schema({
   },
   username: {
     type: String,
-    required: true,
     unique: true,
+    required: true,
   },
   name: {
     type: String,
     required: true,
   },
-  image: {
-    type: String,
-  },
+  image: String,
   bio: String,
   createdBy: {
     type: mongoose.Schema.Types.ObjectId,
@@ -37,7 +34,7 @@ const communitySchema = new mongoose.Schema({
   ],
 });
 
-const User =
+const Community =
   mongoose.models.Community || mongoose.model("Community", communitySchema);
 
-export default User;
+export default Community;
