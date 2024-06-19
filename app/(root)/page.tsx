46b032532569f95e998f795e952acc90511636b0
prathamdupare <prathamdupare@proton.me>
2024-06-20 02:20:41 +0530

    minor fix

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
 
   return (
     <>
-      <h1 className="head-text text-left ">Home</h1>
+      <h1 className="head-text text-left ">-Home-</h1>
 
       <section className="mt-9 flex flex-col gap-10 ">
         {result.posts.length === 0 ? (
